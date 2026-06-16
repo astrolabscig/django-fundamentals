66370e12409f4656c201dfd92aed0914698734f1
astrolab <astrolabscig@gmail.com>
2026-06-16 18:25:54 +0000

    Add create_post and update_post view functions

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/create/', views.create_post, name='create_post'),
+    path('post/<slug:slug>/edit', views.update_post, name='update_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from blog.forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
@@ -13,3 +14,33 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:home')
+    else:
+        form = PostForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/post_form.html', context)
+
+def update_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_detail', slug=post.slug)
+    else:
+        form = PostForm(instance=post)
+        
+    context = {
+        'form': form,
+        'post': post
+    }
+    return render(request, 'blog/post_form.html', context)
