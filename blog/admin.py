1fbb6dfd764d04cef0ad1cdf1567aeb65eaa4902
astrolab <astrolabscig@gmail.com>
2026-06-16 17:29:38 +0000

    Register the Post model with Django admin and customize the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at')
+    search_fields = ('title', 'body')
+    list_filter  = ('category', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
